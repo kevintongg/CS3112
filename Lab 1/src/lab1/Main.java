b1644caa8f0ace348a103c95a29217506b45b59f
Kevin Tong <kcr1230@hotmail.com>
2016-08-25 13:59:32 -0700

    asdf

--- Lab 1/src/lab1/Main.java
+++ Lab 1/src/lab1/Main.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 
 public class Main {
 
-    private static final Scanner sc = new Scanner(System.in);
-
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
 
         int choice;
 
