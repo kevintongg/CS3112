1c2e992ff2a819151be0035b243f4def2b374a28
Kevin Tong <kcr1230@hotmail.com>
2016-08-24 23:33:46 -0700

    asdf

---
+++ Notes/src/DFS.js
@@ -0,0 +1,41 @@
+// tree {
+//   int val
+//   node left
+//   node right
+// }
+
+// Recursive
+function dfs(tree, val) {
+    if (tree === null) {
+        return false;
+    }
+
+    if (tree.val === search) {
+        return true;
+    }
+
+    return (dfs(tree.left, search) || dfs(tree.right, search));
+}
+
+// Iterative
+function dfs(tree, val) {
+    let stack = []
+
+    stack.push(tree);
+    while (stack.length > 0) {
+        let node = stack.pop();
+
+        if (node.val === val) {
+            return true;
+        }
+
+        if (node.left) {
+            stack.push(node.left);
+        }
+        if (node.right) {
+            stack.push(node.right);
+        }
+    }
+
+    return false;
+}
