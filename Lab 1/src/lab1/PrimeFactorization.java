28f0e8e18e39fb88f98998d8c5c1f0f0e7194f4d
Kevin Tong <kcr1230@hotmail.com>
2016-09-27 14:52:15 -0700

    Spacing and method name

--- Lab 1/src/lab1/PrimeFactorization.java
+++ Lab 1/src/lab1/PrimeFactorization.java
@@ -4,25 +4,25 @@ import java.util.Scanner;
 
 public class PrimeFactorization {
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+    Scanner sc = new Scanner(System.in);
 
-        int n;
-        int counter;
+    int n;
+    int counter;
 
-        System.out.println("Enter a positive number for your prime factorization:");
-        n = sc.nextInt();
+    System.out.println("Enter a positive number for your prime factorization:");
+    n = sc.nextInt();
 
-        for (int i = 2; i <= n; i++) {
-            counter = 0;
-            while (n % i == 0) {
-                n /= i;
-                counter++;
-            }
-            if (counter != 0) {
-                System.out.println(i + "^" + counter);
-            }
-        }
+    for (int i = 2; i <= n; i++) {
+      counter = 0;
+      while (n % i == 0) {
+        n /= i;
+        counter++;
+      }
+      if (counter != 0) {
+        System.out.println(i + "^" + counter);
+      }
     }
+  }
 }
--- Lab 1/src/lab1/Sieve.java
+++ Lab 1/src/lab1/Sieve.java
@@ -4,31 +4,31 @@ import java.util.Scanner;
 
 public class Sieve {
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+    Scanner sc = new Scanner(System.in);
 
-        int n;
+    int n;
 
-        System.out.println("Enter a positive number to sieve:");
-        n = sc.nextInt();
+    System.out.println("Enter a positive number to sieve:");
+    n = sc.nextInt();
+  }
+
+  private static int sieve(int n) {
+
+    boolean[] primes = new boolean[n];
+    double j;
+    int count = 1;
+
+    for (int p = 2; p < n; p++) {
+      primes[n] = true;
+    }
+    for (int p = 2; p < Math.sqrt(n); p++) {
+      if (primes[n]) {
+        j = Math.pow(p, 2);
+      }
     }
 
-    private static int sieve(int n) {
-
-        boolean[] primes = new boolean[n];
-        double j;
-        int count = 1;
-
-        for (int p = 2; p < n; p++) {
-            primes[n] = true;
-        }
-        for (int p = 2; p < Math.sqrt(n); p++) {
-            if (primes[n]) {
-                j = Math.pow(p, 2);
-            }
-        }
-
-        return count;
-    }
+    return count;
+  }
 }
