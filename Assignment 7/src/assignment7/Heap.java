7226cf3d1451c5b5c919a0daaf361c551662a740
Kevin Tong <kcr1230@hotmail.com>
2016-11-15 13:15:43 -0800

    Renaming

---
+++ Assignment 7/src/assignment7/Heap.java
@@ -0,0 +1,104 @@
+package assignment7;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Heap<E extends Comparable<E>> {
+
+  private List<E> list = new ArrayList<>();
+
+  /**
+   * Create a default heap
+   */
+  Heap() {
+  }
+
+  /**
+   * Create a heap from an array of objects
+   */
+  public Heap(E[] objects) {
+    for (E object : objects) {
+      add(object);
+    }
+  }
+
+  /**
+   * Add a new object into the heap
+   */
+  void add(E newObject) {
+
+    // Append to the heap
+    list.add(newObject);
+    // The index of last NODE
+    int currentIndex = list.size() - 1;
+
+    while (currentIndex > 0) {
+      int parentIndex = (currentIndex - 1) / 2;
+      // Swap if current object is greater than its parent
+      if (list.get(currentIndex).compareTo(list.get(parentIndex)) > 0) {
+        E temp = list.get(currentIndex);
+        list.set(currentIndex, list.get(parentIndex));
+        list.set(parentIndex, temp);
+      } else {
+        break;
+      }
+
+      currentIndex = parentIndex;
+    }
+  }
+
+  /**
+   * Remove the root from the heap
+   */
+  E remove() {
+
+    if (list.size() == 0) {
+      return null;
+    }
+
+    E removedObject = list.get(0);
+    list.set(0, list.get(list.size() - 1));
+    list.remove(list.size() - 1);
+
+    int currentIndex = 0;
+    while (currentIndex < list.size()) {
+      int leftChildIndex = 2 * currentIndex + 1;
+      int rightChildIndex = 2 * currentIndex + 2;
+
+      // Find the max between two children
+      if (leftChildIndex >= list.size()) {
+        // The tree is a heap
+        break;
+      }
+      int maxIndex = leftChildIndex;
+      if (rightChildIndex < list.size()) {
+        // Compare two children
+        if (list.get(maxIndex).compareTo(list.get(rightChildIndex)) < 0) {
+          maxIndex = rightChildIndex;
+        }
+      }
+
+      if (list.get(currentIndex).compareTo(list.get(maxIndex)) < 0) {
+        E temp = list.get(maxIndex);
+        // Swap with larger child
+        list.set(maxIndex, list.get(currentIndex));
+        list.set(currentIndex, temp);
+        currentIndex = maxIndex;
+      } else {
+        // The tree is a heap
+        break;
+      }
+    }
+
+    return removedObject;
+  }
+
+  /**
+   * Get the number of nodes in the tree
+   */
+  public int getSize() {
+
+    return list.size();
+
+  }
+}
---
+++ Assignment 7/src/assignment7/HeapNode.java
@@ -0,0 +1,12 @@
+package assignment7;
+
+class HeapNode {
+
+  final int NODE;
+  final int LIST_NUM;
+
+  HeapNode(int node, int listNum) {
+    this.NODE = node;
+    this.LIST_NUM = listNum;
+  }
+}
